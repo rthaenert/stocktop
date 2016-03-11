853025a6f86838182d5863ce16bc3186283c661d
Ralf Thaenert <ralf.thaenert@googlemail.com>
2016-03-11 19:00:05 +0100

    add header row, add some more stocks, extend README.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # stocktop
+stocktop is a text-based user interface for monitoring stocks/indices/currencies intraday.
+Currently stocktop uses the deprecated but still working google finance api (https://developers.google.com/finance/)
 
-stocktop is a text-based user interface for monitoring stocks (similar to htop for processes)
+## Running stocktop
+stocktop runs with Python 2.7.
+
+	pip install googlefinance
+	pip install urwid
+
+Afterwards stocktop can be started with:
+	python stocktop.py
+ 
--- stocktop.py
+++ stocktop.py
@@ -5,18 +5,19 @@ import googlefinance
 import json
 import decimal
 
-stock_symbols = ["AAPL", "TSLA", "T", "GOOGL", "TWTR"]
+stock_symbols = ["AAPL", "TSLA", "T", "GOOGL", "TWTR", "GE", "MSFT", "BAC"]
 data = {}
 quote_rows = []
 
 class QuoteRow(urwid.WidgetWrap):
-	def __init__(self, symbol, last_quote):
+	def __init__(self, symbol, last_quote, exchange):
 		self.symbol_text = urwid.AttrMap(urwid.Text(symbol), 'quote_default')
 		self.last_quote_text = urwid.AttrMap(urwid.Text(last_quote), 'quote_default')
-		self.urwid_columns = urwid.Columns([self.symbol_text, self.last_quote_text])
+		self.exchange= urwid.AttrMap(urwid.Text(exchange), 'quote_default')
+		self.urwid_columns = urwid.Columns([self.symbol_text, self.last_quote_text, self.exchange])
 		urwid.WidgetWrap.__init__(self, self.urwid_columns)
 
-	def update_quote(self, new_quote):
+	def update_quote(self, new_quote, exchange):
 		last = decimal.Decimal(self.last_quote_text.original_widget.get_text()[0])
 		new = decimal.Decimal(new_quote)
 		if new == last:
@@ -26,6 +27,7 @@ class QuoteRow(urwid.WidgetWrap):
 		else:
 			self.last_quote_text.set_attr_map({None: 'quote_lower'})
 		self.last_quote_text.original_widget.set_text(new_quote)
+		self.exchange.original_widget.set_text(exchange)
 	
 	def get_symbol(self):
 		return self.symbol_text.original_widget.get_text()[0]
@@ -38,16 +40,16 @@ def handle_input(input):
 def update_quotes(stock_symbols):
 	quotes = googlefinance.getQuotes(stock_symbols)
 	for quote in quotes:
-		data[ quote["StockSymbol"] ] = quote["LastTradePrice"] 
+		data[ quote["StockSymbol"] ] = {"LastPrice": quote["LastTradePrice"],
+						"Exchange" : quote["Index"] }
 	return data
 
 def test_callback(urwid_main_loop, user_data):
 	quotes = update_quotes(stock_symbols)
-	# TODO improve this
 	for quote_row in quote_rows:
 		for symbol, quote in quotes.iteritems():
 			if quote_row[1].get_symbol() == symbol:
-				quote_row[1].update_quote(quote)
+				quote_row[1].update_quote(quote["LastPrice"], quote["Exchange"])
 	urwid_main_loop.draw_screen()
 	urwid_main_loop.set_alarm_in(5, test_callback)
 
@@ -59,15 +61,15 @@ def main():
 	if len(args.symbols) > 0:
 		stock_symbols = args.symbols
 	quotes = update_quotes(stock_symbols)
-	quote_rows = []
-	for symbol, quote in quotes.iteritems():
-		quote_rows.append(('pack', QuoteRow(symbol, quote)))
+	quote_rows = [('pack', QuoteRow("Symbol", "LastPrice", "Exchange"))]
+	for symbol, symbol_data in quotes.iteritems():
+		quote_rows.append(('pack', QuoteRow(symbol, symbol_data["LastPrice"], symbol_data["Exchange"])))
 
 	palette = [('quote_higher','dark green', 'black', 'standout'),
 		   ('quote_lower', 'dark red', 'black', 'standout'),
 		   ('quote_default', 'white', 'black', 'standout')]
 
-	loop = urwid.MainLoop(urwid.Filler(urwid.Pile(quote_rows)), palette, unhandled_input=handle_input)
+	loop = urwid.MainLoop(urwid.Filler(urwid.Pile(quote_rows), 'top'), palette, unhandled_input=handle_input)
 	loop.set_alarm_in(5, test_callback)
 	loop.run()
 
