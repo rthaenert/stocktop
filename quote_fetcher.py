9e6f46761a326e9ab56f3228efd12d68b93de721
Ralf Thaenert <ralf.thaenert@googlemail.com>
2016-03-14 19:54:38 +0100

    remove googlefinance, display %change and last price datetime

--- quote_fetcher.py
+++ quote_fetcher.py
@@ -1,46 +1,22 @@
-import googlefinance
 import yahoo_finance
 import time
 import urllib2
 
 yahoo_time_format = "%Y-%m-%d %H:%M:%S %Z"
-google_time_format = "%Y-%m-%dT%H:%M%SZ"
 
-def parse_datetime(date_time_str, source):
-	yahoo_datetime = time.strptime(last_trade_date_time, yahoo_time_format)
-	google_lasttime = quotes_merged[symbol]["LastTradeDateTime"]
-	if source == "google":
-		return time.strptime(date_time_str, google_time_format)
-	else:	# currently only yahoo
-		last_trade_date_time, offset_str = yahoo_datetime[:-5], yahoo_datetime[-5:]
-		# TODO handle UTC offset correctly, currently its left out
-		return time.strptime(last_trade_date_time, yahoo_time_format)
-
-def update_quote_google(quotes_dict, new_quote):
-	symbol = new_quote["StockSymbol"]
-	if not quotes_dict[symbol] or quotes_dict[symbol]["LastTradeDateTime"] == "":
-		quotes_dict[symbol] = {
-			"Exchange" : new_quote["Index"],
-			"LastPrice": new_quote["LastTradePrice"].replace(',',''),
-			"LastTradeDateTime": new_quote["LastTradeDateTime"],
-			"ChangeInPercent": new_quote["ChangePercent"],
-			"Source": "google"
-		}
-	else: 
-		pass
-	return quotes_dict	
+def parse_datetime(date_time_str):
+	last_trade_date_time, offset_str = date_time_str[:-5], date_time_str[-5:]
+	# TODO handle UTC offset correctly, currently its left out
+	return time.strptime(last_trade_date_time, yahoo_time_format)
 
 def update_quote_yahoo(quotes_dict, symbol):
-	if not quotes_dict[symbol] or quotes_dict[symbol]["LastTradeDateTime"]=="":
-		# look up quote
-		quote = yahoo_finance.Share(symbol)
-		quotes_dict[symbol] = {
-			"Exchange" : quote.get_stock_exchange(),
-			"LastPrice": quote.get_price().replace(',',''),
-			"LastTradeDateTime": quote.get_trade_datetime(),
-			"ChangeInPercent": quote.get_change(),
-			"Source" : "yahoo"
-		}
+	quote = yahoo_finance.Share(symbol)
+	quotes_dict[symbol] = {
+		"Exchange" : quote.get_stock_exchange(),
+		"LastPrice": quote.get_price().replace(',',''),
+		"LastTradeDateTime": quote.get_trade_datetime(),
+		"ChangeInPercent": quote.get_change_in_percent(),
+	}
 	return quotes_dict	
 
 def get_quotes(symbols):
@@ -53,12 +29,6 @@ def get_quotes(symbols):
 		"LastTradeDateTime" : "",
 		"ChangeInPercent": "",
 		"Exchange": ""} for symbol in symbols}	
-	try:
-		quotes_google = googlefinance.getQuotes(symbols)
-		map(lambda new_quote : update_quote_google(quotes_merged, new_quote), quotes_google)
-	except urllib2.HTTPError as e:
-		# TODO log this error
-		pass
 	map(lambda symbol: update_quote_yahoo(quotes_merged, symbol), symbols)
 	return quotes_merged
 
--- stocktop.py
+++ stocktop.py
@@ -5,7 +5,7 @@ import json
 import decimal
 import quote_fetcher
 
-symbols = ["^GDAXI", "^GSPC", ".INX", "AAPL", "TSLA", "T", "GOOGL", "TWTR", "GE", "MSFT", "BAC"]
+symbols = ["^GDAXI", "^GSPC",  "AAPL", "TSLA", "T", "GOOGL", "TWTR", "GE", "MSFT", "BAC"]
 
 data = {}
 quote_rows = []
@@ -13,27 +13,37 @@ quote_rows = []
 UPDATE_INTERVAL=5
 
 class QuoteRow(urwid.WidgetWrap):
-	def __init__(self, symbol, last_quote, exchange):
-		self.symbol_text = urwid.AttrMap(urwid.Text(symbol), 'quote_default')
-		self.last_quote_text = urwid.AttrMap(urwid.Text(last_quote), 'quote_default')
-		self.exchange= urwid.AttrMap(urwid.Text(exchange), 'quote_default')
-		self.urwid_columns = urwid.Columns([self.symbol_text, self.last_quote_text, self.exchange])
+
+	def __init__(self, symbol, last_quote, last_quote_date_time, percent_change, exchange):
+		self.text_fields = {
+			"symbol": urwid.AttrMap(urwid.Text(symbol), 'quote_default'),
+			"last_quote" : urwid.AttrMap(urwid.Text(last_quote), 'quote_default'),
+			"last_quote_date_time":urwid.AttrMap(urwid.Text(last_quote_date_time), 'quote_default'),
+			"percent_change" : urwid.AttrMap(urwid.Text(percent_change), 'quote_default'),
+			"exchange":urwid.AttrMap(urwid.Text(exchange), 'quote_default')
+		}
+		self.urwid_columns = urwid.Columns([self.text_fields["symbol"],
+							self.text_fields["last_quote"],
+							self.text_fields["last_quote_date_time"],
+							self.text_fields["percent_change"],
+							self.text_fields["exchange"]])
 		urwid.WidgetWrap.__init__(self, self.urwid_columns)
 
-	def update_quote(self, new_quote, exchange):
-		last = decimal.Decimal(self.last_quote_text.original_widget.get_text()[0])
+	def update_quote(self, new_quote, last_quote_date_time, percent_change, exchange):
+		last = decimal.Decimal(self.text_fields["last_quote"].original_widget.get_text()[0])
 		new = decimal.Decimal(new_quote)
 		if new == last:
-			self.last_quote_text.set_attr_map({None: 'quote_default'})
+			self.text_fields["last_quote"].set_attr_map({None: 'quote_default'})
 		elif new > last:
-			self.last_quote_text.set_attr_map({None: 'quote_higher'})
+			self.text_fields["last_quote"].set_attr_map({None: 'quote_higher'})
 		else:
-			self.last_quote_text.set_attr_map({None: 'quote_lower'})
-		self.last_quote_text.original_widget.set_text(new_quote)
-		self.exchange.original_widget.set_text(exchange)
+			self.text_fields["last_quote"].set_attr_map({None: 'quote_lower'})
+		self.text_fields["last_quote"].original_widget.set_text(new_quote)
+		self.text_fields["last_quote_date_time"].original_widget.set_text(last_quote_date_time)
+		self.text_fields["percent_change"].original_widget.set_text(percent_change)
 	
 	def get_symbol(self):
-		return self.symbol_text.original_widget.get_text()[0]
+		return self.text_fields["symbol"].original_widget.get_text()[0]
 
 def handle_input(input):
 	if input == "esc":
@@ -45,21 +55,25 @@ def fetch_quotes(urwid_main_loop, user_data):
 	for quote_row in quote_rows:
 		for symbol, quote in quotes.iteritems():
 			if quote_row[1].get_symbol() == symbol:
-				quote_row[1].update_quote(quote["LastPrice"], quote["Exchange"])
+				quote_row[1].update_quote(quote["LastPrice"], 
+							quote["LastTradeDateTime"],
+							quote["ChangeInPercent"],
+							quote["Exchange"])
 	urwid_main_loop.draw_screen()
 	urwid_main_loop.set_alarm_in(UPDATE_INTERVAL, fetch_quotes)
 
 def main():
-	parser = argparse.ArgumentParser(description="Show live stocks")
+	parser = argparse.ArgumentParser(description="Show live quotes for Stocks/Indices/Currencies")
 	parser.add_argument('symbols', metavar='stock-symbol', type=str, nargs='*', help='ticker symbols')
 	args = parser.parse_args()
 	global symbols, quote_rows
 	if len(args.symbols) > 0:
 		symbols = args.symbols
 	quotes = quote_fetcher.get_quotes(symbols)
-	quote_rows = [('pack', QuoteRow("Symbol", "LastPrice", "Exchange"))]
+	quote_rows = [('pack', QuoteRow("Symbol", "LastPrice", "LastPriceDateTime", "Change %", "Exchange"))]
 	for symbol, symbol_data in quotes.iteritems():
-		quote_rows.append(('pack', QuoteRow(symbol, symbol_data["LastPrice"], symbol_data["Exchange"])))
+		quote_rows.append(('pack', QuoteRow(symbol, symbol_data["LastPrice"], 
+				symbol_data["LastTradeDateTime"], symbol_data["ChangeInPercent"], symbol_data["Exchange"])))
 
 	palette = [('quote_higher','dark green', 'black', 'standout'),
 		   ('quote_lower', 'dark red', 'black', 'standout'),
